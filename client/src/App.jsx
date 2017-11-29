376347328bd4fc2934f8f6c41da81c32163ce571
Nick Desai <nickdesai@Nicks-MacBook-Pro-2.local>
2017-11-28 22:24:52 -0500

    moved navbar to separate component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,34 +4,12 @@ import HomePage from "./pages/HomePage";
 import UploadPage from "./pages/UploadPage";
 import VisualizePage from "./pages/VisualizePage";
 import NotFound from "./pages/NotFound";
+import Navigation from "./pages/components/Navigation";
 
 import {Route, Link, BrowserRouter as Router, Switch} from "react-router-dom";
 import {LinkContainer, IndexLinkContainer} from "react-router-bootstrap";
 import {Navbar, NavItem, Nav} from "react-bootstrap";
 
-const Navigation = () => (
-    <Navbar inverse>
-        <Navbar.Brand>
-            <Link to="/"> Financialyzr</Link>
-        </Navbar.Brand>
-        <Navbar.Collapse>
-            <Nav>
-                <IndexLinkContainer to="/">
-                    <NavItem>Home</NavItem>
-                </IndexLinkContainer>
-                <LinkContainer to="/upload" exact>
-                    <NavItem>Upload</NavItem>
-                </LinkContainer>
-                <LinkContainer to="/visualize" exact>
-                    <NavItem>Visualize</NavItem>
-                </LinkContainer>
-            </Nav>
-            <Nav pullRight>
-                <NavItem href="/auth/logout">Logout</NavItem>
-            </Nav>
-        </Navbar.Collapse>
-    </Navbar>
-);
 
 const Routes = () => (
     <div>
---
+++ client/src/pages/components/Navigation.jsx
@@ -0,0 +1,30 @@
+import React, {Component} from "react";
+import {Navbar, Nav, NavItem} from "react-bootstrap";
+import {Link} from "react-router-dom";
+import {IndexLinkContainer, LinkContainer} from "react-router-bootstrap";
+
+const Navigation = () => (
+    <Navbar inverse>
+        <Navbar.Brand>
+            <Link to="/"> Financialyzr</Link>
+        </Navbar.Brand>
+        <Navbar.Collapse>
+            <Nav>
+                <IndexLinkContainer to="/">
+                    <NavItem>Home</NavItem>
+                </IndexLinkContainer>
+                <LinkContainer to="/upload" exact>
+                    <NavItem>Upload</NavItem>
+                </LinkContainer>
+                <LinkContainer to="/visualize" exact>
+                    <NavItem>Visualize</NavItem>
+                </LinkContainer>
+            </Nav>
+            <Nav pullRight>
+                <NavItem href="/auth/logout">Logout</NavItem>
+            </Nav>
+        </Navbar.Collapse>
+    </Navbar>
+);
+
+export default Navigation;
